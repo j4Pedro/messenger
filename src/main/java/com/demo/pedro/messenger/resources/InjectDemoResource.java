b4778f2164578d01c31daeed0798bb80a00133fa
Pedro <pedro@cadip.com.tw>
2022-03-22 12:23:01 +0800

    try 3 types of parameter

---
+++ src/main/java/com/demo/pedro/messenger/resources/InjectDemoResource.java
@@ -0,0 +1,25 @@
+package com.demo.pedro.messenger.resources;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.CookieParam;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.MatrixParam;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/injectdemo")
+@Consumes(MediaType.TEXT_PLAIN)
+@Produces(MediaType.TEXT_PLAIN)
+public class InjectDemoResource {
+
+	@GET
+	@Path("annotations")
+	public String getParamsUsingAnnotations(@MatrixParam("param") String matrixParam,
+											@HeaderParam("authSessionID") String header,
+											@CookieParam("JSESSIONID")String cookie) {
+		return "matrixParam : " + matrixParam + ", header : " + header + ", cookie: " + cookie;
+	}
+
+}
