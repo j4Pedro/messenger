5b358fa4ee7e2a05eb9e5ca82b8eba8a67e06462
Pedro <pedro@cadip.com.tw>
2022-03-22 09:57:16 +0800

    change getProfiles in Databaseclass

--- src/main/java/com/demo/pedro/messenger/database/DatabaseClass.java
+++ src/main/java/com/demo/pedro/messenger/database/DatabaseClass.java
@@ -7,17 +7,16 @@ import com.demo.pedro.messenger.model.Message;
 import com.demo.pedro.messenger.model.Profile;
 
 public class DatabaseClass {
-	
-	public static Map<Long,Message> messages = new HashMap<>();	
-	public static Map<Long,Profile> profiles = new HashMap<>();
-	
-	
+
+	public static Map<Long, Message> messages = new HashMap<>();
+	public static Map<String, Profile> profiles = new HashMap<>();
+
 	public static Map<Long, Message> getMessages() {
 		return messages;
 	}
-	public static Map<Long, Profile> getProfiles() {
+
+	public static Map<String, Profile> getProfiles() {
 		return profiles;
-	}	
-	
-	
+	}
+
 }
