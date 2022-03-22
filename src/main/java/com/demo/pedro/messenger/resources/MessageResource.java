a2666cdc768814e4980691c07495826a47e43bf1
Pedro <pedro@cadip.com.tw>
2022-03-22 09:17:39 +0800

    move annotation to top

--- src/main/java/com/demo/pedro/messenger/resources/MessageResource.java
+++ src/main/java/com/demo/pedro/messenger/resources/MessageResource.java
@@ -6,34 +6,47 @@ import com.demo.pedro.messenger.model.Message;
 import com.demo.pedro.messenger.service.MessageService;
 
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
-	
+
 	MessageService messageService = new MessageService();
-	
+
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public ArrayList<Message> getMessages() {
 		return messageService.getAllMessages();
 	}
-	
+
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message message) {
 		return messageService.addMessage(message);
-	} 
+	}
+
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long id, Message message) {
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
 	
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messageService.removeMessage(id);
+	}
+
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") long id) {
 		return messageService.getMessage(id);
 //		return "get path param : "+messageId;
