87c3bc63f85f6da9ca8130b7f0d28b4c0d2391d2
Pedro <pedro@cadip.com.tw>
2022-03-22 14:00:06 +0800

    use other 3 type of parameter

--- src/main/java/com/demo/pedro/messenger/resources/InjectDemoResource.java
+++ src/main/java/com/demo/pedro/messenger/resources/InjectDemoResource.java
@@ -7,7 +7,10 @@ import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.MatrixParam;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.UriInfo;
 
 @Path("/injectdemo")
 @Consumes(MediaType.TEXT_PLAIN)
@@ -17,9 +20,16 @@ public class InjectDemoResource {
 	@GET
 	@Path("annotations")
 	public String getParamsUsingAnnotations(@MatrixParam("param") String matrixParam,
-											@HeaderParam("authSessionID") String header,
-											@CookieParam("JSESSIONID")String cookie) {
+			@HeaderParam("authSessionID") String header, @CookieParam("JSESSIONID") String cookie) {
 		return "matrixParam : " + matrixParam + ", header : " + header + ", cookie: " + cookie;
+	}
+
+	@GET
+	@Path("context")
+	public String getParamUsingContext(@Context UriInfo uriInfo,@Context HttpHeaders headers) {
+		String path = uriInfo.getAbsolutePath().toString();
+		String cookies = headers.getCookies().toString();
+		return "path: " + path+", cookie= "+cookies;
 	}
 
 }
