370b5e7cbbbc3feee2d862b62d44b0da9a3aa9ac
Pedro <pedro@cadip.com.tw>
2022-03-23 10:52:10 +0800

    Accept more content

--- src/main/java/com/demo/pedro/messenger/resources/MessageResource.java
+++ src/main/java/com/demo/pedro/messenger/resources/MessageResource.java
@@ -20,37 +20,48 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.core.UriInfo;
-import javassist.CtNewMethod;
 
 @Path("/messages")
 @Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
+@Produces(value = { MediaType.APPLICATION_JSON, MediaType.TEXT_XML })
 public class MessageResource {
 
 	MessageService messageService = new MessageService();
 
 	@GET
-	public List<Message> getMessages(@BeanParam MessageFilterBean fileterBean) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Message> getJsonMessages(@BeanParam MessageFilterBean filterBean) {
+		System.out.println("Json method.");
+		if (filterBean.getYear() > 0) {
+			return messageService.getAllMessageForYear(filterBean.getYear());
+		}
+		if (filterBean.getStart() >=0 && filterBean.getSize() >= 0) {
+			return messageService.getAllMessagesPabinated(filterBean.getStart(), filterBean.getSize());
+		}
+		return messageService.getAllMessages();
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public List<Message> getXmlMessages(@BeanParam MessageFilterBean fileterBean) {
+		System.out.println("Xml method.");
 		if (fileterBean.getYear() > 0) {
 			return messageService.getAllMessageForYear(fileterBean.getYear());
 		}
-		if (fileterBean.getStart() > 0 && fileterBean.getSize() > 0) {
+		if (fileterBean.getStart() >=0 && fileterBean.getSize() >= 0) {
 			return messageService.getAllMessagesPabinated(fileterBean.getStart(), fileterBean.getSize());
 		}
 		return messageService.getAllMessages();
 	}
 
 	@POST
-	public Response addMessage(Message message,@Context UriInfo uriInfo) throws URISyntaxException {
+	public Response addMessage(Message message, @Context UriInfo uriInfo) throws URISyntaxException {
 		Message newMessage = messageService.addMessage(message);
 		String newId = String.valueOf(newMessage.getId());
 		URI uri = uriInfo.getAbsolutePathBuilder().path(newId).build();
-		return Response.created(uri)
-				.entity(newMessage)
-				.build();
-		
+		return Response.created(uri).entity(newMessage).build();
+
 //		return Response.status(Status.CREATED)
 //				.entity(newMessage)
 //				.build();
@@ -65,50 +76,46 @@ public class MessageResource {
 	}
 
 	@DELETE
-	@Path("/{messageId}") 
+	@Path("/{messageId}")
 	public void deleteMessage(@PathParam("messageId") long id) {
 		messageService.removeMessage(id);
 	}
 
 	@GET
 	@Path("/{messageId}")
-	public Message getMessage(@PathParam("messageId") long id,@Context UriInfo uriInfo) {
+	public Message getMessage(@PathParam("messageId") long id, @Context UriInfo uriInfo) {
 		Message message = messageService.getMessage(id);
-		message.addLink(getUriForSelf(uriInfo, message),"self");
-		message.addLink(getUriForProfile(uriInfo, message),"profile");
-		message.addLink(getUriForComments(uriInfo, message),"comments");
+		message.addLink(getUriForSelf(uriInfo, message), "self");
+		message.addLink(getUriForProfile(uriInfo, message), "profile");
+		message.addLink(getUriForComments(uriInfo, message), "comments");
 		return message;
 //		return messageService.getMessage(id);
 //		return "get path param : "+messageId;
 	}
 
 	private String getUriForComments(UriInfo uriInfo, Message message) {
-		URI uri = uriInfo.getBaseUriBuilder()			// http://localhost:8080/messenger/webapi/	
-				.path(MessageResource.class) 
-				.path(MessageResource.class,"getCommentResource")   			//		  /			
-				.path(CommentResource.class) 
-				.resolveTemplate("messageId", message.getId())
-				.build();
+		URI uri = uriInfo.getBaseUriBuilder() // http://localhost:8080/messenger/webapi/
+				.path(MessageResource.class).path(MessageResource.class, "getCommentResource") // /
+				.path(CommentResource.class).resolveTemplate("messageId", message.getId()).build();
 		return uri.toString();
 	}
 
 	private String getUriForProfile(UriInfo uriInfo, Message message) {
-		URI uri = uriInfo.getBaseUriBuilder()		// http://localhost:8080/messenger/webapi/
-		.path(ProfileResource.class)  													//		  /profiles
-		.path(message.getAuthor())					 											//		  /{autthorName}
-		.build();
+		URI uri = uriInfo.getBaseUriBuilder() // http://localhost:8080/messenger/webapi/
+				.path(ProfileResource.class) // /profiles
+				.path(message.getAuthor()) // /{autthorName}
+				.build();
 		return uri.toString();
 	}
 
 	private String getUriForSelf(UriInfo uriInfo, Message message) {
-		String uri = uriInfo.getBaseUriBuilder()		// http://localhost:8080/messenger/webapi/
-		.path(MessageResource.class)  													//		  /messages
-		.path(Long.toString(message.getId()))					 								//		  /{messageId}
-		.build()
-		.toString();
+		String uri = uriInfo.getBaseUriBuilder() // http://localhost:8080/messenger/webapi/
+				.path(MessageResource.class) // /messages
+				.path(Long.toString(message.getId())) // /{messageId}
+				.build().toString();
 		return uri;
 	}
-	
+
 	@Path("/{messageId}/comments")
 	public CommentResource getCommentResource() {
 		return new CommentResource();
